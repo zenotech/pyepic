ce0aa7a9d5f032c3d43c6284d9caec9f28590fe2
Mike Turner <mike.turner@zenotech.com>
2021-05-17 12:01:53 +0100

    Updates to Desktop API calls

--- pyepic/desktops/desktop.py
+++ pyepic/desktops/desktop.py
@@ -32,50 +32,40 @@
 
 from enum import Enum
 
-from epiccore.models import (
-    DesktopNodeQuote,
-    DesktopNodeLaunchSpec,
-    DataSpec
+from epiccore.models import DesktopNodeQuote, DesktopNodeLaunchSpec, DataSpec
 
-)
 
-class MountType (Enum):
-    """ How should the data folder be mounted to the desktop. Offline takes a copy of the data and will not be automatically synced back to the data store.	
-    """
-    ONLINE = 'online'
-    OFFLINE = 'offline'
+class MountType(Enum):
+    """How should the data folder be mounted to the desktop. Offline takes a copy of the data and will not be automatically synced back to the data store."""
+
+    ONLINE = "online"
+    OFFLINE = "offline"
 
 
 class Desktop(object):
-    """ An EPIC Desktop Definition
+    """An EPIC Desktop Definition
 
-            :param data_path: The path to the data to be loaded on the Desktop, formed as an epic url (e.g. "epic://path_to/data")
-            :type path: str
-            :param application_version: The ID of the Desktop Application Version to launch
-            :type application_version: int
-            :param node_type: The ID of the Desktop Node Type to launch
-            :type node_type: int
-            :param connection_type: The ID of the Desktop Connection Type to use
-            :type connection_type: int
+    :param data_path: The path to the data to be loaded on the Desktop, formed as an epic url (e.g. "epic://path_to/data")
+    :type path: str
+    :param node_code: The node_code of the Desktop Node Type to launch
+    :type node_code: str
 
-            :var runtime: The runtime for the Desktop in hours
-            :vartype runtime: int
-            :var mount_type: How should the data folder be mounted to the desktop. Offline takes a copy of the data and will not be automatically synced back to the data store.	
-            :vartype mount_type: :class:`MountType`
-            :var secure_ip: Should we restrict which IPs can connect to this node? (defaults to False)
-            :vartype secure_ip: bool, optional
-            :var ip_address: If secure_ip is True, which IP should connections be restricted to?
-            :vartype ip_address: str, optional
-            :var invoice_reference: Reference string for this desktop to appear on invoices.
-            :vartype invoice_reference: str, optional
-            :var project_id: ID of the EPIC project to run this job in
-            :vartype project_id: int, optional
+    :var runtime: The runtime for the Desktop in hours
+    :vartype runtime: int
+    :var mount_type: How should the data folder be mounted to the desktop. Offline takes a copy of the data and will not be automatically synced back to the data store.
+    :vartype mount_type: :class:`MountType`
+    :var secure_ip: Should we restrict which IPs can connect to this node? (defaults to False)
+    :vartype secure_ip: bool, optional
+    :var ip_address: If secure_ip is True, which IP should connections be restricted to?
+    :vartype ip_address: str, optional
+    :var invoice_reference: Reference string for this desktop to appear on invoices.
+    :vartype invoice_reference: str, optional
+    :var project_id: ID of the EPIC project to run this job in
+    :vartype project_id: int, optional
     """
 
-    def __init__(self, data_path, application_version = 1, node_type = 1 , connection_type = 1):
-        self.application_version = application_version
-        self.node_type = node_type
-        self.connection_type = connection_type
+    def __init__(self, data_path, node_code):
+        self.node_code = node_code
         self.data_path = data_path
         self.runtime = 1
         self.mount_type = MountType.ONLINE
@@ -87,27 +77,23 @@ class Desktop(object):
     def get_quote_spec(self):
         """Get a Specification for this desktop for quotes
 
-            :return: Desktop Quote Specification
-            :rtype: class:`epiccore.models.DesktopNodeQuote`
+        :return: Desktop Quote Specification
+        :rtype: class:`epiccore.models.DesktopNodeQuote`
         """
         quote = DesktopNodeQuote(
-            application_version=self.application_version,
-            node_type=self.node_type,
-            connection_type=self.connection_type,
-            runtime=self.runtime
+            node_code=self.node_code,
+            runtime=self.runtime,
         )
         return quote
 
     def get_launch_spec(self):
         """Get a Specification for this desktop for launching it
 
-            :return: Desktop Specification
-            :rtype: class:`epiccore.models.DesktopNodeLaunchSpec`
+        :return: Desktop Specification
+        :rtype: class:`epiccore.models.DesktopNodeLaunchSpec`
         """
         spec = DesktopNodeLaunchSpec(
-            application_version=self.application_version,
-            node_type=self.node_type, 
-            connection_type=self.connection_type,
+            node_code=self.node_code,
             runtime=self.runtime,
             secure_ip=self.secure_ip,
             ip_address=self.ip_address,
@@ -116,7 +102,6 @@ class Desktop(object):
                 path=self.data_path,
             ),
             mount_type=self.mount_type.value,
-            project=self.project_id
+            project=self.project_id,
         )
         return spec
-
